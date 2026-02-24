0f1622dce08653c166156a300b53971c26e64faa
Fei Yu <maivanvuong@gmail.com>
2026-02-24 10:10:41 +0000

    fix: add ShowStatus import for improved functionality

--- KKPExProvider/src/main/kotlin/com/example/KKPExProvider.kt
+++ KKPExProvider/src/main/kotlin/com/example/KKPExProvider.kt
@@ -3,6 +3,7 @@ package com.example
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.lagradost.cloudstream3.*
 import com.lagradost.cloudstream3.Score
+import com.lagradost.cloudstream3.ShowStatus
 import com.lagradost.cloudstream3.utils.*
 import com.lagradost.cloudstream3.utils.AppUtils.parseJson
 import android.content.Context
