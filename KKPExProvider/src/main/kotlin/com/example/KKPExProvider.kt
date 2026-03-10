d5d8ecf9054be5d74756028644df5bfc35b03804
Fei Yu <maivanvuong@gmail.com>
2026-03-10 17:28:38 +0700

    Update KKPExProvider.kt

--- KKPExProvider/src/main/kotlin/com/example/KKPExProvider.kt
+++ KKPExProvider/src/main/kotlin/com/example/KKPExProvider.kt
@@ -1,9 +1,11 @@
 package com.example
 
-import com.fasterxml.jackson.annotation.JsonProperty
 import com.lagradost.cloudstream3.*
+import com.lagradost.cloudstream3.Score
 import com.lagradost.cloudstream3.utils.*
 import com.lagradost.cloudstream3.utils.AppUtils.parseJson
+import com.fasterxml.jackson.annotation.JsonProperty
+import java.util.Locale
 import android.content.Context
 
 class KKPExProvider : MainAPI() {
