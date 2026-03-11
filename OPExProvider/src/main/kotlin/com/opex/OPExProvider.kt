d94e9a8d0449279b1b4e284bcb11ae6134c7da8d
Fei Yu <maivanvuong@gmail.com>
2026-03-11 19:28:27 +0700

    Update OPExProvider.kt

--- OPExProvider/src/main/kotlin/com/opex/OPExProvider.kt
+++ OPExProvider/src/main/kotlin/com/opex/OPExProvider.kt
@@ -147,7 +147,7 @@ class OPExProvider : MainAPI() {
                 metaTags.add(if (epTotal.isNotEmpty() && !cleanCurrent.contains("/")) "$cleanCurrent/$epTotal Tập" else epCurrent)
             }
         }
-        movie.lang?.split("+")?.forEach { val t = it.trim(); if (t.isNotEmpty()) metaTags.add(t) }
+        movie.lang?.let { l -> l.split("+").forEach { if (!it.contains("Vietsub", true)) metaTags.add(it.trim()) } }
         movie.category?.forEach { it.name?.let { n -> metaTags.add(n) } }
 
         // --- FIX CHỌN SERVER: Gộp Vietsub/Thuyết minh ---
