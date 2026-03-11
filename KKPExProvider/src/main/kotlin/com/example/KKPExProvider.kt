84c85085c3a09afc953b65e1fa0667f2fe2a702a
Fei Yu <maivanvuong@gmail.com>
2026-03-11 09:41:39 +0700

    Update KKPExProvider.kt

--- KKPExProvider/src/main/kotlin/com/example/KKPExProvider.kt
+++ KKPExProvider/src/main/kotlin/com/example/KKPExProvider.kt
@@ -239,18 +239,19 @@ class KKPExProvider : MainAPI() {
         
         // ==========================================
         //Score
-         // --- LẤY ĐIỂM SỐ VÀ DIỄN VIÊN TRỰC TIẾP TỪ TMDB ---
         var tmdbLiveScore: Double? = null
-        var tmdbActors: List<ActorData>? = null
-        
-        val tmdbId = movie.tmdb?.id
-        val tmdbType = if (isSeries) "tv" else "movie"
-
         if (!tmdbId.isNullOrEmpty()) {
             try {
-                // 1. Gọi lấy Điểm số
-                val infoUrl = "https://api.themoviedb.org/3/$tmdbType/$tmdbId?api_key=$tmdbApiKey&language=vi-VN"
-                tmdbLiveScore = app.get(infoUrl).parsedSafe<TmdbInfo>()?.voteAverage
+                val tmdbInfoUrl = "https://api.themoviedb.org/3/$tmdbType/$tmdbId?api_key=$tmdbApiKey&language=vi-VN"
+                if (isSeries) {
+                    val tvInfo = app.get(tmdbInfoUrl).parsedSafe<TmdbTvInfo>()
+                    tmdbLiveScore = tvInfo?.voteAverage
+                } else {
+                    val movieInfo = app.get(tmdbInfoUrl).parsedSafe<TmdbMovieInfo>()
+                    tmdbLiveScore = movieInfo?.voteAverage
+                }
+            } catch (e: Exception) {}
+        }
         //---------
                 return if (isSeries) {  
             newTvSeriesLoadResponse(movie.name ?: "", url, TvType.TvSeries, episodesList) {
@@ -392,6 +393,12 @@ data class TmdbCreditsResponse(
     val cast: List<TmdbCast>? = null
 )
 
-data class TmdbInfo(
+// Cho phim bộ
+data class TmdbTvInfo(
+    @param:JsonProperty("vote_average") val voteAverage: Double? = null
+)
+
+// Cho phim lẻ
+data class TmdbMovieInfo(
     @param:JsonProperty("vote_average") val voteAverage: Double? = null
 )
