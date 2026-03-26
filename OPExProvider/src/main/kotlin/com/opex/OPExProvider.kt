6db8d5a46d2d7674112e00538676765af201a395
Fei Yu <maivanvuong@gmail.com>
2026-03-26 09:28:24 +0700

    Update OPExProvider.kt

--- OPExProvider/src/main/kotlin/com/opex/OPExProvider.kt
+++ OPExProvider/src/main/kotlin/com/opex/OPExProvider.kt
@@ -185,10 +185,10 @@ val categorySlugs = movie.category?.mapNotNull { it.slug }?.joinToString(",") ?:
 // 3. Khởi tạo danh sách đề xuất rỗng
 //var recommendationsList = emptyList<SearchResponse>()
 val recommendationsList = if (countrySlug.isNotEmpty()) {
-    val recUrl = "$mainUrl/v1/api/quoc-gia/$countrySlug?limit=30&category=$categorySlugs&sort_field=year&sort_type=desc"
+    val recUrl = "$mainUrl/v1/api/quoc-gia/$countrySlug?limit=20&category=$categorySlugs&sort_field=year&sort_type=desc"
     
     // Gọi hàm có sẵn và lọc bỏ phim hiện tại để không tự đề xuất chính nó
-    getListFromUrl(recUrl).filter { it.name != movieName } 
+    getListFromUrl(recUrl)
 } else {
     emptyList()
 }
