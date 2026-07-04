bbb82ed3ef035b979ed8ac7990b9edf74309ccfd
Fei Yu <maivanvuong@gmail.com>
2026-07-05 06:24:27 +0700

    Update build.gradle.kts

--- DailymotionProvider/build.gradle.kts
+++ DailymotionProvider/build.gradle.kts
@@ -1,3 +1,7 @@
+dependencies {
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+}
 // Use an integer for version numbers
 version = 10
 
